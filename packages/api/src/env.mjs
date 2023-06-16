a4c66f077b853b6e23d3c908334a3c43989836fd
Philitician <philiprene89@gmail.com>
2023-06-16 16:59:12 +0200

    Initial commit

---
+++ packages/api/src/env.mjs
@@ -0,0 +1,33 @@
+import { createEnv } from "@t3-oss/env-nextjs";
+import * as z from "zod";
+
+export const env = createEnv({
+  server: {
+    NEXTJS_URL: z.preprocess(
+      (str) =>
+        process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : str,
+      process.env.VERCEL_URL ? z.string().min(1) : z.string().url(),
+    ),
+
+    STRIPE_API_KEY: z.string(),
+  },
+  client: {
+    NEXT_PUBLIC_STRIPE_STD_PRODUCT_ID: z.string(),
+    NEXT_PUBLIC_STRIPE_STD_MONTHLY_PRICE_ID: z.string(),
+    NEXT_PUBLIC_STRIPE_PRO_PRODUCT_ID: z.string(),
+    NEXT_PUBLIC_STRIPE_PRO_MONTHLY_PRICE_ID: z.string(),
+  },
+  runtimeEnv: {
+    NEXTJS_URL: process.env.NEXTJS_URL,
+    STRIPE_API_KEY: process.env.STRIPE_API_KEY,
+    NEXT_PUBLIC_STRIPE_STD_PRODUCT_ID:
+      process.env.NEXT_PUBLIC_STRIPE_STD_PRODUCT_ID,
+    NEXT_PUBLIC_STRIPE_STD_MONTHLY_PRICE_ID:
+      process.env.NEXT_PUBLIC_STRIPE_STD_MONTHLY_PRICE_ID,
+    NEXT_PUBLIC_STRIPE_PRO_PRODUCT_ID:
+      process.env.NEXT_PUBLIC_STRIPE_PRO_PRODUCT_ID,
+    NEXT_PUBLIC_STRIPE_PRO_MONTHLY_PRICE_ID:
+      process.env.NEXT_PUBLIC_STRIPE_PRO_MONTHLY_PRICE_ID,
+  },
+  skipValidation: !!process.env.SKIP_ENV_VALIDATION,
+});
